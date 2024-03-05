43e95af77706d9e7850892940a7b5d01fa1a5223
mercy ngure <mercyngure87@gmail.com>
2024-03-05 12:45:27 +0300

    update on app

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ function formatDate(date) {
 }
 
 function searchCity(city) {
-  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiKey = "b247067e50befodba3ebc71534fa405t";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
   axios.get(apiUrl).then(updateWeather);
 }
